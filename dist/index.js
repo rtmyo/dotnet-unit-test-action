d31a6fab16cfbb8bdb46fb866f53720858377c3d
rtmyo <rafael.tamayo@omnitracs.com>
2022-10-25 23:45:51 -0500

    Use vercel/ncc

--- dist/index.js
+++ dist/index.js
@@ -9688,9 +9688,10 @@ const core = __nccwpck_require__(3935);
 const github = __nccwpck_require__(6433);
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
+  const exit_code = core.get('exit-code');
   console.log(`Hello ${nameToGreet}!`);
+  console.log(exit_code);
+
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
+  const exit_code = core.get('exit-code');
   console.log(`Hello ${nameToGreet}!`);
+  console.log(exit_code);
+
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
